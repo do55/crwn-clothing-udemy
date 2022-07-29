cffa2a86a5214d1f4d7c7662f029948dcf3c6341
Dominika Profus <dominika.profus@gmail.com>
2022-07-29 13:06:37 +0200

    cart-item added

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,14 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+
 import "./product-card.styles.scss";
 import Button from "../button/buttton.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -11,7 +17,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to Card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to Card
+      </Button>
     </div>
   );
 };
