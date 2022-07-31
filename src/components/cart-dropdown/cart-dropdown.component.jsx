b3c5ec418f2d2d214faae0cf5635e7d9aac50994
Dominika Profus <dominika.profus@gmail.com>
2022-07-31 18:10:50 +0200

    created checkoutpage

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,9 +3,16 @@ import { CartContext } from "../../context/cart.context";
 import Button from "../button/buttton.component";
 import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -15,7 +22,7 @@ const CartDropdown = () => {
       </div>
       )
       <div />
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
